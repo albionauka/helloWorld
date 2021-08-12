497070d01891dfda7f5d92b7a44d3d415b03ee15
albionauka <albionauka@gmail.com>
2021-08-12 14:14:45 -0700

    completed variable workshop

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+var firstName = "Albiona";
+let lastName = "Uka";
+const age = 17;
+
+firstName = "my dude";
+lastName = "what's up";
+
+console.log('Hello ' + firstName + ' '+ lastName + '!' + ' You are ' + age + ' years old');
